eb715dc91a9154363277fbf481664351752570f5
Yuki Sakamoto <yuki.sakamoto@riken.jp>
2021-09-06 16:42:10 +0900

    add journals

--- convert_bibtex.rb
+++ convert_bibtex.rb
@@ -12,6 +12,8 @@ ConvTable = {
   "Physical Review E" => "Phys. Rev. E.",
   "Physical Review Letters" => "Phys. Rev. Lett.",
 
+  "Journal of the Physical Society of Japan" => "J. Phys. Soc. Japan",
+
   # ACS
   "Journal of the American Chemical Society" => "J. Am. Chem. Soc.",
   "Journal of organometallic chemistry" => "J. Organomet. Chem.",
@@ -22,12 +24,37 @@ ConvTable = {
   "The Journal of Physical Chemistry C" => "J. Phys. Chem. C",
   "The Journal of Physical Chemistry Letters" => "J. Phys. Chem. Lett.",
   "Chemical Reviews" => "Chem. Rev.",
+  "ACS Applied Materials \\& Interfaces" => "ACS Appl. Mater. Interfaces",
+  "Nano Letters" => "Nano Lett.",
 
+  # Elsevier
+  "Chemical Physics Letters" => "Chem. Phys. Lett.",
+  "Computer Physics Communications" => "Comput. Phys. Commun.",
+
+  # nature and science
+  "Nature Photonics" => "Nat. Photonics",
+  "Nature" => "Nature",
+  "Science" => "Science",
+
+
+  #RSC
+  "Chemical Society Reviews" => "Chem. Soc. Rev.",
+
+  "Solid State Communications" => "Solid State Commun.",
+  "IEEE Journal of Quantum Electronics" => "IEEE J. Quantum Electron.",
+  "Advanced Materials" => "Adv. Mater.",
+  "Laser \\& Photonics Reviews" => "Laser Photonics Rev.",
+  "Nanomaterials" => "Nanomaterials",
+  "Sensors and Actuators B: Chemical" => "Sens. Actuators B Chem.",
+  "Sensors and Actuators" => "Sens. Actuators",
+  "Journal of Electroanalytical Chemistry and Interfacial Electrochemistry" => "J. Electroanal. Chem. Interf. Electrochem.", "Journal of the Optical Society of America" => "J. Opt. Soc. Am.", "The London, Edinburgh, and Dublin Philosophical Magazine and Journal of Science" => "London, Edinburgh Dublin Philos. Mag. J. Sci.", "Zeitschrift für Physik A Hadrons and nuclei" => "Z. Phys. A",
+
+  "Applied Physics Letters"  => "Appl. Phys. Lett.",
   "Journal of Applied Crystallography"  => "J. Appl. Crystallogr.",
   "Computational Materials Science"     => "Comp. Mat. Sci.",
   "Journal of Physics: Condensed Matter"  => "J. Phys. Condens. Matter",
   "Journal of Computational Chemistry"  => "J. Comput. Chem.",
-  "The Journal of Chemical Physics"     => "J. Chem. Phys",
+  "The Journal of Chemical Physics"     => "J. Chem. Phys.",
   "International Journal of Quantum Chemistry" => "Int. J. Quantum Chem.",
 
   "Bulletin of the Chemical Society of Japan" => "Bull. Chem. Soc. Jpn.",
@@ -39,7 +66,7 @@ ConvTable = {
 }
 
 
-def convert(filename, convert_table, case_sensitive = true)
+def convert(filename, convert_table, case_sensitive = true, no_title = false)
   b = BibTeX.open(filename)
   b_articles = b['@article']
   b.each do |item|
@@ -49,6 +76,10 @@ def convert(filename, convert_table, case_sensitive = true)
         item.journal = convert_table[item.journal]
       else
         STDERR.puts "warning: #{item.key} * '#{item.journal}' not found"
+      end
+
+      if no_title == true
+        item.title = ""
       end
     else  # Not article
       STDERR.puts "warning: #{item.key} : skip (not @article) "
@@ -60,9 +91,17 @@ end
 
 
 if __FILE__ == $0
+  no_title = false
+  case_sensitive = true
+  opt = OptionParser.new
+  opt.on('-i', 'ignore case'){|v| case_sensitive = false}
+  opt.on('--no-title'){|v| no_title = true}
+  opt.parse!(ARGV)
+  STDERR.puts "Disable Title : #{no_title}"
   file_list = ARGV
+  
   file_list.each do |file|
-    convert(file, ConvTable)
+    convert(file, ConvTable, case_sensitive, no_title)
   end
   #puts ConvTable.to_yaml
 end
